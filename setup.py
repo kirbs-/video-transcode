ecca4e2756250a5242b9cfb8f5a264ee30e5fb5e
kirbs- <kirbycm@gmail.com>
2019-08-24 20:10:04 -0400

    Added makefile and setup info.

--- setup.py
+++ setup.py
@@ -50,14 +50,12 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
 
     # What does your project relate to?
-    keywords='jupyter notebook presentation',
+    keywords='raspberrypi mqtt GPIO',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -70,33 +68,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['dotmap', 'pyyaml', 'paho-mqtt', 'Adafruit_DHT'],
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={
-    #     'hide_code': ['*.js','*.txt', os.path.join('Templates', '*'), 'hide_code_config.json'],
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_po
-    # scripts=['hide_code/hide_code.py'],
-    # cmdclass={'install': install},
-
-    # entry_points={
-    #     'nbconvert.exporters': [
-    #         'hide_code_html = hide_code:HideCodeHTMLExporter',
-    #         'hide_code_pdf = hide_code:HideCodePDFExporter',
-    #         # 'hide_code_latexpdf = hide_code:HideCodeLatexPDFExporter',
-    #         'hide_code_latex = hide_code:HideCodeLatexExporter',
-    #         'hide_code_slides = hide_code:HideCodeSlidesExporter'
-    #     ],
-    # }
+    entry_points={
+        'console_scripts': ['rpi2mqtt=rpi2mqtt.event_loop:main']
+    }
 )
