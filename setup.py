ab6bfa12af2b6fcbfae8441934b444102ebbd137
Chris Kirby <kirbycm@gmail.com>
2026-06-17 15:08:47 -0400

    Version bump and pinned moviepy

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 
 setup(name='video_transcode',
-      version='1.6.0',
+      version='1.7.0',
       url='https://github.com/kirbs-/video-transcode',
       description='Video commercial cutting and transcoding stack.',
       author='Chris Kirby',
@@ -11,7 +11,7 @@ setup(name='video_transcode',
       license='MIT',
       packages=['video_transcode'],
       package_data={'video_transcode': [os.path.join('config', '*')]},
-      install_requires=['celery==4.4.7', 'pyyaml>=5.1', 'pendulum', 'moviepy', 'redis'],
+      install_requires=['celery==4.4.7', 'pyyaml>=5.1', 'pendulum', 'moviepy==1.0.3', 'redis'],
       zip_safe=False,
       entry_points={
         'console_scripts': ['video-transcode=video_transcode.video_transcode:main']
